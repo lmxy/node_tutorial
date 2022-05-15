3561432f2f14aeeadc43c8598266588c2b5f2c09
lmxy <32830461+lmxy@users.noreply.github.com>
2022-05-15 21:19:00 +0800

    5hours
    begin to learn node after JS tutorial

---
+++ Node_tutorial/02-globals.js
@@ -0,0 +1,11 @@
+// globals - no window !!!!!
+
+// __dirname - path to current directory
+// __filename - file name
+// require   - function to use modules (Common JS)
+// module    - info about current module (file)
+// process   - info about env where the program is being excuted
+console.log(__dirname)
+setInterval(() => {
+  console.log('hello world')
+}, 1000)
