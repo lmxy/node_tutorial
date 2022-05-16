f275ea2ef41f679b14c34abe5e8be95bc412804e
lmxy <wjzxyaowei@163.com>
2022-05-16 16:15:59 +0800

    first commit

--- Node_tutorial/app.js
+++ Node_tutorial/app.js
@@ -1,21 +1,20 @@
-const http = require('http');
+// npm - global command, comes with node
+// npm --version
 
-const server = http.createServer((req, res) => {
-  if(req.url === '/'){
-    res.write('Welcome to our home page');
-    res.end();
-  }
-  else if(req.url === '/about'){
-    res.write('Here is our short history');
-    res.end();
-  }
-  else{
-    res.write(`
-    <h1>Oops!</h1>
-    <p>We can't seem to find the page you are looking for</p>
-    <a href="/">back home</a>
-    `);
-    res.end();
-  }
-}).listen(5001)
-//
+// local dependency - use it only in this particular project
+// npm i <packageName>
+
+// global dependency - use it any project
+// npm install -g <packageName>
+// sudo npm install -g <packageName> (mac)
+
+// package.json - manifest file (stores important info about project/package)
+// manual approach (create package.json in the root, create properties etc)
+// npm init (step by step, press enter to skip)
+// npm init -y (everything default)
+
+const _ = require('lodash');
+
+const items = [1, [2, [3, [4]]]];
+const newItems = _.flattenDeep(items);
+console.log(newItems);
